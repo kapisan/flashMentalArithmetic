caa581e2365c031fb59b1f6864826f061edd1338
kapisan <60970645 + kapisan @ users.noreply.github.com>
2021-07-14 09:58:04 +0900

    VCのコードを日本語で記述

--- FlashMentalArithmeticApp/ViewController.swift
+++ FlashMentalArithmeticApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var displayLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func startActionButton(_ sender: UIButton) {
+        //ラベルに表示する箱を準備
+        var displayLabelNumber: Int = 0
+        //ランダムな数字を作った時に入れる変数を準備
+        //ランダムな数字を作成
+        //ランダムな数字を表示（2秒間）
+        //表示回数が5回以下ならループ（5回表示したら画面遷移）
+
+    }
 
 }
 
