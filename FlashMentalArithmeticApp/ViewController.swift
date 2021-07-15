d54064a707c8b27a9a67d9ca183d15a6abd87627
kapisan <60970645 + kapisan @ users.noreply.github.com>
2021-07-15 21:23:34 +0900

    乱数を生成したらrandomArrayへ加える

--- FlashMentalArithmeticApp/ViewController.swift
+++ FlashMentalArithmeticApp/ViewController.swift
@@ -18,14 +18,16 @@ class ViewController: UIViewController {
 
     @IBAction func startActionButton(_ sender: UIButton) {
         //ラベルに表示する箱を準備
-        var displayLabelNumber:Int = 0
+//        var displayLabelNumber:Int = 0
+        var randomArray = [Int]()
         //ランダムな数字を表示（2秒間）
         //表示回数が5回以下ならループ（5回表示したら画面遷移）
         for _ in 1 ..< 6 {
             //ランダムな数字を生成
-           var randomNumber = Int.random(in: 1..<9)
+            let randomNumber = Int.random(in: 1..<9)
+            randomArray.append(randomNumber)
         }
-
+        print(randomArray)
     }
 
 }
