744955d30647d5628bb1cd0f111466264b8c52b3
kapisan <60970645 + kapisan @ users.noreply.github.com>
2021-07-20 21:55:20 +0900

    1秒毎にランダムな数字が被らないように表示できるようにした

--- FlashMentalArithmeticApp/ViewController.swift
+++ FlashMentalArithmeticApp/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     @IBOutlet private weak var displayLabel: UILabel!
 
-    var wRandomArray = [Int]()
+    var numberArray = [Int]()
+    var newNumberArray = [Int]()
     var count = 0
     var x = 0
     var btnTimer: Timer!
@@ -19,11 +20,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        for _ in 1 ..< 6 {
-            //ランダムな数字を生成
-            let randomNumber = Int.random(in: 1..<9)
-            wRandomArray.append(randomNumber)
-        }
+        numberArray = [1,2,3,4,5,6,7,8,9]
+        newNumberArray = numberArray.shuffled()
+
+        print(newNumberArray)
 
     }
 
@@ -32,75 +32,33 @@ class ViewController: UIViewController {
     }
 
     @objc func changeColor() {
-        x += 1
-//        if x == 1 {
-//            displayLabel.text = String(wRandomArray[0])
-//        } else if x == 2 {
-//            displayLabel.text = String(wRandomArray[1])
-//        } else if x == 3 {
-//            displayLabel.text = String(wRandomArray[2])
-//        } else if x == 4 {
-//            displayLabel.text = String(wRandomArray[3])
-//        } else if x == 5 {
-//            displayLabel.text = String(wRandomArray[4])
-//            x = 0
-//            self.btnTimer.invalidate()
-//        }
 
-        switch x {
+        count += 1
+
+        switch count {
         case 1:
-            displayLabel.text = String(wRandomArray[0])
+            displayLabel.text = String(newNumberArray[0])
         case 2:
-            displayLabel.text = String(wRandomArray[1])
+            displayLabel.text = String(newNumberArray[1])
         case 3:
-            displayLabel.text = String(wRandomArray[2])
+            displayLabel.text = String(newNumberArray[2])
         case 4:
-            displayLabel.text = String(wRandomArray[3])
+            displayLabel.text = String(newNumberArray[3])
         case 5:
-            displayLabel.text = String(wRandomArray[4])
+            displayLabel.text = String(newNumberArray[4])
         case 6:
-            x = 0
+//            x = 0
+            count = 0
             self.btnTimer.invalidate()
             displayLabel.text = ""
+            numberArray.removeAll()
             print("画面遷移")
         default:
             print("対象外")
         }
     }
     @IBAction func startActionButton(_ sender: UIButton) {
-
         startTimer()
-        //ラベルに表示する箱を準備
-//        var displayLabelNumber:Int = 0
-//        var randomArray = [Int]()
-        //ランダムな数字を表示（2秒間）
-        //表示回数が5回以下ならループ（5回表示したら画面遷移）
-//        for _ in 1 ..< 6 {
-//            //ランダムな数字を生成
-//            let randomNumber = Int.random(in: 1..<9)
-//            randomArray.append(randomNumber)
-//        }
-        print(wRandomArray)
-
-//        displayLabel.text = String(wRandomArray[count])
-//
-//        for _ in 0 ..< 4 {
-//            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-//                self.count += 1
-//                self.displayLabel.text = String(self.wRandomArray[self.count])
-//            }
-//        }
-
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-//            self.count += 1
-//            self.displayLabel.text = String(self.wRandomArray[self.count])
-//        }
-
-//        displayLabel.text = String(wRandomArray[count])
-//        count += 1
     }
 
-    
-
 }
-
