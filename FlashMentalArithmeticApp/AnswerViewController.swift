c709fab48f4cda6b55008c8228e5644f2ea3ff7d
kapisan <60970645 + kapisan @ users.noreply.github.com>
2021-07-21 10:35:28 +0900

    キーボードの高さに合わせてTextFieldの高さを変更&キーボードを閉じる処理を追加

--- FlashMentalArithmeticApp/AnswerViewController.swift
+++ FlashMentalArithmeticApp/AnswerViewController.swift
@@ -7,27 +7,51 @@
 
 import UIKit
 
-class AnswerViewController: UIViewController {
+class AnswerViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var answerTextField: UITextField!
+    
     var answerArray = [Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationController?.setNavigationBarHidden(true, animated: false)
+        self.answerTextField.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
 
         print(answerArray)
     }
-    
 
-    /*
-    // MARK: - Navigation
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //キーボードを閉じる
+        textField.resignFirstResponder()
+
+        return true
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            } else {
+                let suggestionHeight = self.view.frame.origin.y + keyboardSize.height
+                self.view.frame.origin.y -= suggestionHeight
+            }
+        }
+    }
+
+    @objc func keyboardWillHide() {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
 
 }
